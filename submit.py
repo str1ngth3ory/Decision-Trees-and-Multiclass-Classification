b36280e587ad6cd190221ab1283d124153f845cb
Fenu, Stefano Gabriele <sfenu3@gatech.edu>
2017-06-28 01:15:36 -0400

    Su17 updates (#2)
    * Updated with PEP8 and Google style docstrings.
    
    * Updated README.md to include assignment descriptions and problems.
    
    * Updated submit to use nelson module
    
    * Added LoadCSV as root attribute.
    
    * Made additional PEP8 corrections
    
    * Added missing init.
    
    * Added support for future division
    
    * Added local unit tests
    
    * Removed notebook.
    
    * Improved README with additional details and corrected typos.
    
    * Changed entropy requirements to GINI impurity
    
    * Updated tests for gini
    
    * Fixed local gini tests
    
    * pep8 compliance for local test updates

--- submit.py
+++ submit.py
@@ -1,106 +1,46 @@
-import time
-import os
-import sys
-import argparse
-import json
-import datetime
-from bonnie.submission import Submission
+#!/usr/bin/env python
+# coding=utf-8
+from nelson.gtomscs import submit
 
 LATE_POLICY = \
-"""Late Policy:
+    """Late Policy:
 
-  \"I have read the late policy for CS6601. I understand that only my last
-  commit before the late submission deadline will be accepted.\"
-"""
+    \"I have read the late policy for CS6601. I understand that only my last
+    commit before the late submission deadline will be accepted.\"
+  """
 
 HONOR_PLEDGE = \
-"""Honor Pledge:
+    """Honor Pledge:
 
-  \"I have read the Collaboration and Academic Honesty policy for CS6601.
-  I certify that I have used outside references only in accordance with
-  this policy, that I have cited any such references via code comments,
-  and that I have not copied any portion of my submission from another
-  past or current student.\"
-"""
+    \"I have read the Collaboration and Academic Honesty policy for CS6601.
+    I certify that I have used outside references only in accordance with
+    this policy, that I have cited any such references via code comments,
+    and that I have not copied any portion of my submission from another
+    past or current student.\"
+  """
+
 
 def require_pledges():
-  print(LATE_POLICY)
-  ans = raw_input("Please type 'yes' to agree and continue>")
-  if ans != "yes":
-    raise RuntimeError("Late policy not accepted.")
+    print(LATE_POLICY)
+    ans = raw_input("Please type 'yes' to agree and continue>")
 
-  print
-  print(HONOR_PLEDGE)
-  ans = raw_input("Please type 'yes' to agree and continue>")
-  if ans != "yes":
-    raise RuntimeError("Honor pledge not accepted")
-  print
+    if ans != "yes":
+        raise RuntimeError("Late policy not accepted.")
 
-def display_assignment_4_output(submission):
-  timestamp = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
+    print
+    print(HONOR_PLEDGE)
+    ans = raw_input("Please type 'yes' to agree and continue>")
+    if ans != "yes":
+        raise RuntimeError("Honor pledge not accepted")
+    print
 
-  while not submission.poll():
-    time.sleep(3.0)
-
-  if submission.feedback():
-
-    if submission.console():
-        sys.stdout.write(submission.console())
-
-    filename = "%s-result-%s.json" % (submission.quiz_name, timestamp)
-
-    with open(filename, "w") as fd:
-      json.dump(submission.feedback(), fd, indent=4, separators=(',', ': '))
-
-    print("\n(Details available in %s.)" % filename)
-
-  elif submission.error_report():
-    error_report = submission.error_report()
-    print(json.dumps(error_report, indent=4))
-  else:
-    print("Unknown error.")
-
-def display_game(submission):
-  while not submission.poll():
-    time.sleep(3.0)
-
-  if submission.feedback():
-    sys.stdout.write(submission.feedback())
-  elif submission.error_report():
-      error_report = submission.error_report()
-      print(json.dumps(error_report, indent=4))
-  else:
-    print("Unknown error.")
 
 def main():
-  parser = argparse.ArgumentParser(description='Submits code to the Udacity site.')
-  parser.add_argument('part', choices = ['assignment_4'])
-  parser.add_argument('--provider', choices = ['gt', 'udacity'], default = 'gt')
-  parser.add_argument('--environment', choices = ['local', 'development', 'staging', 'production'], default = 'production')
-  parser.add_argument('--add-data', action='store_true', help='Include this flag to add a data.pickle file that will be available on the test server.')
-
-  args = parser.parse_args()
-
-  if args.part == 'assignment_4':
+    filenames = ["decision_trees_submission.py"]
     require_pledges()
-    quiz = 'assignment_4'
-    filenames = ["decision_trees.py"]
 
-    # Add data.pickle if required
-    # if args.add_data:
-    #   filenames.append("data.pickle")
+    submit('cs6601', 'assignment_4', filenames)
 
-  print "Submission processing...\n"
-  submission = Submission('cs6601', quiz,
-                          filenames = filenames,
-                          environment = args.environment,
-                          provider = args.provider)
-
-  if args.part == 'assignment_4':
-    display_assignment_4_output(submission)
-  else:
-    display_game(submission)
 
 if __name__ == '__main__':
-  main()
-
+    main()
